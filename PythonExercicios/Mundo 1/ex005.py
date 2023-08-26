75d083e915d1621885469e1f4731b31d4acb4320
Matheus Borelli <matheus_borelli@hotmail.com>
2023-08-26 12:33:12 -0300

    fazendo o exercico 5 que não tinha sido realizado anteriormente

---
+++ PythonExercicios/Mundo 1/ex005.py
@@ -0,0 +1,5 @@
+'''Exercicio 5: Faça um programa que leia um número Inteiro e mostre na tela o seu sucessor'''
+
+n = int(input('Digite um número: '))
+
+print(f'Analisando o valor {n}, seu sucessor é {n-1} e o sucessor é {n+1}')
