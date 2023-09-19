ca83e330cdfc6d8a616110b65eedd6108a053e58
Matheus Borelli <matheus_borelli@hotmail.com>
2023-09-19 11:09:20 -0300

    Finalizando o mundo 2 de Python do Curso em Video

--- PythonExercicios/Mundo 2/ex064_Tratamento_de_valores_V.1.0.py
+++ PythonExercicios/Mundo 2/ex064_Tratamento_de_valores_V.1.0.py
@@ -2,3 +2,13 @@
  O programa só vai parar quando o usuário digitar o valor 999, que é a condição de parada.
    No final, mostre quantos números foram digitados e qual foi a soma entre eles (desconsiderando o flag)."""
 
+
+num = cont = soma = 0
+num = int(input('Digite um número [999 para parar]: '))
+
+while num != 999:
+  soma += num
+  cont += 1
+  num = int(input('Digite um número [999 para parar]: '))
+
+print(f'Você digitou o {cont} números e a soma entre eles foi {soma}.')
