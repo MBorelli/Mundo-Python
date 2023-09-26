878c313607fa71b3b0f7f1af168c5bd6d68501f7
Matheus Borelli - MacBook <matheus_borelli@hotmail.com>
2023-09-26 15:49:31 -0300

    Resoluçao dos primeiros exercicios da aula 20 - funçoes- de python do curso em video

---
+++ PythonExercicios/Mundo 3/ex097_Um_Print_Especial.py
@@ -0,0 +1,17 @@
+"""Exercicio 97: Faça um programa que tenha uma função chamada escreva(), 
+que receba um texto qualquer como parâmetro e mostre uma mensagem com tamanho adaptável.
+Ex: escreva(‘Olá, Mundo!’) Saída:
+~~~~~~~~~
+Olá, Mundo!
+~~~~~~~~~ """
+
+
+def escreva(msg):
+    tam = len(msg) + 4
+    print('-' * tam) #subistituir - por acento tio.
+    print(f'  {msg}')
+    print('-' * tam) #subistituir - por acento tio.
+
+
+#Programa principal
+escreva('Olá, Mundo!')
