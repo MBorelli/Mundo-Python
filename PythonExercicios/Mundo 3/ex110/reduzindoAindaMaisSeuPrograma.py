e84b29dd3c3f6d678918e2c0592c8d00714119ed
Matheus Borelli <matheus_borelli@hotmail.com>
2023-11-28 16:32:29 -0300

    Adicionando o exercicio 110 do curso em video sobre modularização em Python

---
+++ PythonExercicios/Mundo 3/ex110/reduzindoAindaMaisSeuPrograma.py
@@ -0,0 +1,11 @@
+"""Exercicio 110: Adicione o módulo moeda.py criado nos desafios anteriores, uma função chamada resumo(),
+que mostre na tela algumas informações geradas pelas funções que já temos no módulo criado até aqui."""
+
+
+from moeda import *
+
+p = float(input('Digite o preço: R$'))
+
+moeda(resumo(p, 60, 15))
+
+# Observação: acredito por ser editor de código diferente e por ser epocas diferentes da aula do professor, minha resolução ficou diferente da do professor.
