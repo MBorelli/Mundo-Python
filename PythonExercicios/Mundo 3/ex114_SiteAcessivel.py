ab1864241636bf40560dc4504404cdb8be0b34aa
Matheus Borelli <matheus_borelli@hotmail.com>
2024-01-10 19:05:49 -0300

    Adicionando os exercicios da aula 23 sobre tratamento de erros e exceções em Python do Curso em Video

---
+++ PythonExercicios/Mundo 3/ex114_SiteAcessivel.py
@@ -0,0 +1,14 @@
+"""Exercicio 114: Crie um código em Python que teste se o site pudim está acessível pelo computador usado."""
+
+
+import urllib
+import urllib.request
+
+
+try:
+    site = urllib.request.urlopen('http://www.pudim.com.br')
+except urllib.error.URLError:
+    print('O site Pudim não está acessível no momento.')
+else:
+    print('Consgui acessar o site Pudim com sucesso!')
+    print(site.read())
